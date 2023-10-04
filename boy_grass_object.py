c8cdca66a6be52662b3ff412513c02172981bc86
xorud1210 <jbong0104@naver.com>
2023-10-04 18:46:33 +0900

    소년과 잔디 객체 생성

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -1,6 +1,29 @@
 from pico2d import *
 
+
 # Game object class here
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400, 30)
+
+    def update(self): pass
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = 0 , 90
+        self.frame = 0
+        self.image = load_image('run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += 5
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 100, 0 , 100, 100, self.x, self.y)
+
 
 def handle_events():
     global running
@@ -11,12 +34,42 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
+
+def reset_world():
+    global running
+    global grass
+    global boy
+
+    running = True
+    grass = Grass()
+    boy = Boy()
+
+def update_world():
+    grass.update()
+    boy.update()
+    pass
+
+
+def render_world():
+    clear_canvas()
+    grass.draw()
+    boy.draw()
+    update_canvas()
+
+
 open_canvas()
 
 # initialization code
+reset_world()
 
 # game main loop code
+while running:
+    handle_events()
+    update_world()  # game logic
+    render_world()  # draw game world
+    delay(0.05)
 
 # finalization code
+
 
 close_canvas()
